ed40a1d28426c12a143510cd0ca453badfa15ffc
Reuben27 <reuben.sd@iitgn.ac.in>
2021-01-14 13:03:04 +0530

    Filter added

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,63 @@ var options = {
 };
 
 var userList = new List('users', options);
+
+$('.filter-electrical').on('click',function(){
+    //var $text = $(this).text();
+    //console.log($text);
+    if($(this).hasClass( 'selected' )){
+        userList.filter();
+        $(this).removeClass('selected');
+    } 
+    else {
+        userList.filter(function(item) {
+            return (item.values().Branch == "Branch: Electrical Engineering");
+        });
+        $(this).addClass('selected');
+    }
+});
+
+$('.filter-chemical').on('click',function(){
+    //var $text = $(this).text();
+    //console.log($text);
+    if($(this).hasClass( 'selected' )){
+        userList.filter();
+        $(this).removeClass('selected');
+    } 
+    else {
+        userList.filter(function(item) {
+            return (item.values().Branch == "Branch: Chemical Engineering");
+        });
+        $(this).addClass('selected');
+    }
+});
+
+$('.filter-civil').on('click',function(){
+    //var $text = $(this).text();
+    //console.log($text);
+    if($(this).hasClass( 'selected' )){
+        userList.filter();
+        $(this).removeClass('selected');
+    } 
+    else {
+        userList.filter(function(item) {
+            return (item.values().Branch == "Branch: Civil Engineering");
+        });
+        $(this).addClass('selected');
+    }
+});
+
+$('.filter-mechanical').on('click',function(){
+    //var $text = $(this).text();
+    //console.log($text);
+    if($(this).hasClass( 'selected' )){
+        userList.filter();
+        $(this).removeClass('selected');
+    } 
+    else {
+        userList.filter(function(item) {
+            return (item.values().Branch == "Branch: Mechanical Engineering");
+        });
+        $(this).addClass('selected');
+    }
+});
