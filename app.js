dc3a69a3746eaa67a0fe3f0aa8f8ce06e1a0c265
Reuben27 <reuben.sd@iitgn.ac.in>
2021-01-18 11:57:49 +0530

    Added login authentication

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express  = require("express");
+const cookieParser = require('cookie-parser');
 const app = express(); 
 const ejs = require('ejs'); 
 const bodyParser = require("body-parser");
@@ -7,7 +8,13 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '473236609433-bsunp8lbeannucdc8m0om4ueph6iaue6.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
+
 app.set ("view engine", "ejs");
+app.use(express.json());
+app.use(cookieParser());
 
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { promisify } = require('util');
@@ -45,7 +52,31 @@ async function displaySpreadsheet(){
     console.log(database);
 }
 
-app.get("/", async (req,res) => {
+app.get("/login", (req,res) => {
+    res.render("login");
+});
+
+app.post("/login", (req,res) => {
+    let token = req.body.token;
+    //console.log(token);
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+        //console.log(payload);
+    }
+    verify().then(() => {
+        res.cookie('session-token', token);
+        res.send('success');
+    }).catch(console.error);
+});
+
+//displaySpreadsheet();
+
+app.get("/", checkAuthenticated, async (req,res) => {
     var database = await accessSpreadsheet();
     //database = [{"name": "Reuben"}, {"name": "Aditya"}];
     //console.log(database);
@@ -53,7 +84,33 @@ app.get("/", async (req,res) => {
     res.render("index", {database: database});
 });
 
-//displaySpreadsheet();
+app.get('/logout', (req, res)=>{
+    res.clearCookie('session-token');
+    res.redirect('/login');
+})
+
+function checkAuthenticated(req, res, next){
+    let token = req.cookies['session-token'];
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID, 
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+    verify()
+    .then(()=>{
+        req.user = user;
+        next();
+    })
+    .catch(err=>{
+        res.redirect('/login');
+    })
+}
 
 app.listen(port, function (){
     console.log("Server running");
--- public/js/index.js
+++ public/js/index.js
@@ -19,6 +19,21 @@ $('.filter-electrical').on('click',function(){
     }
 });
 
+$('.filter-computer').on('click',function(){
+    //var $text = $(this).text();
+    //console.log($text);
+    if($(this).hasClass( 'selected' )){
+        userList.filter();
+        $(this).removeClass('selected');
+    } 
+    else {
+        userList.filter(function(item) {
+            return (item.values().Branch == "Branch: Computer Science and Engineering");
+        });
+        $(this).addClass('selected');
+    }
+});
+
 $('.filter-chemical').on('click',function(){
     //var $text = $(this).text();
     //console.log($text);
@@ -59,6 +74,21 @@ $('.filter-mechanical').on('click',function(){
     else {
         userList.filter(function(item) {
             return (item.values().Branch == "Branch: Mechanical Engineering");
+        });
+        $(this).addClass('selected');
+    }
+});
+
+$('.filter-material').on('click',function(){
+    //var $text = $(this).text();
+    //console.log($text);
+    if($(this).hasClass( 'selected' )){
+        userList.filter();
+        $(this).removeClass('selected');
+    } 
+    else {
+        userList.filter(function(item) {
+            return (item.values().Branch == "Branch: Materials Science and Engineering");
         });
         $(this).addClass('selected');
     }
